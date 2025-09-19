c51c567da3e0eaca62d927f0f7c077ff2f351492
optimisticlucio <73440828+optimisticlucio@users.noreply.github.com>
2025-09-19 11:01:51 +0300

    Started work on commandline tool

--- import-tool/src/lib.rs
+++ import-tool/src/lib.rs
@@ -1,2 +1,13 @@
+use std::io;
+
 mod art_archive;
 mod characters;
+
+pub fn read_line() -> Result<String, io::Error>{
+    let mut input_string = String::new();
+
+    io::stdin()
+        .read_line(&mut input_string)?;
+
+    return Ok(input_string);
+}
--- import-tool/src/main.rs
+++ import-tool/src/main.rs
@@ -1,3 +1,35 @@
+use import_tool::read_line;
+use owo_colors::OwoColorize;
+
 fn main() {
-    println!("Hello, world!");
+    println!(
+"{}
+This tool should be used regularly in testing and only {} on the final site.
+
+{}",
+"Hello! Welcome to the Power Down Wiki's Import Tool!".blue(),
+"once".red().bold().underline(), 
+"If there's any bugs, message Lucio over discord with screenshots.".yellow().italic());
+    
+    println!(
+"\nWhat would you like to import?
+(1) Characters.
+(2) Art.
+(3) Stories.
+(9) Everything!
+(0) Close tool"
+    );
+    loop {
+        let chosen_option = read_line().unwrap();
+
+        let trimmed_option = chosen_option.trim();
+
+        match trimmed_option {
+            "0" => {
+                println!("Goodbye!");
+                break;
+            }
+            _ => println!("{}", "I didn't quite get that.".yellow())
+        }
+    }
 }
