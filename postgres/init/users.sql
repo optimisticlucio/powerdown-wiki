9881a6a0d09bbee782907560ba0edd64ae401597
optimisticlucio <73440828+optimisticlucio@users.noreply.github.com>
2025-11-16 18:49:58 +0200

    starting setup for user management

--- postgres/init/users.sql
+++ postgres/init/users.sql
@@ -4,6 +4,12 @@ CREATE TYPE user_type AS ENUM (
     'superadmin'
 );
 
+CREATE TYPE oauth_provider AS ENUM (
+    'discord',
+    'google',
+    'github'
+)
+
 CREATE TABLE site_user (
     id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY, -- TODO: Should this be random IDs? Identity is sequential.
 
@@ -12,14 +18,29 @@ CREATE TABLE site_user (
     -- TODO: Add pfp.
 
     -- TODO: Think of relevant fields.
-    -- TODO: Add OpenID stuff for login.
+);
+
+CREATE TABLE user_oauth (
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+
+    user_id int NOT NULL 
+        REFERENCES site_user(id)
+        ON DELETE CASCADE,
+    
+    provider oauth_provider NOT NULL,
+
+    provider_user_id TEXT NOT NULL,
+
+    UNIQUE(oauth_provider, provider_user_id),
+    UNIQUE(oauth_provider, user_id)
 );
 
 CREATE TABLE user_session (
     user_id int NOT NULL
-        REFERENCES site_user(id),
+        REFERENCES site_user(id)
+        ON DELETE CASCADE,
     
-    session_id text NOT NULL UNIQUE, -- Session ID should be a long, random string.
+    session_id text PRIMARY KEY, -- Session ID should be a long, random string.
 
     creation_time timestamp with time zone NOT NULL DEFAULT NOW(), -- For the love of god don't set this manually.
     -- The server, whenever reading the session, should check if it's been enough time since the creation for the session to be invalid. If it is, delete the entry.
--- rust/src/user/structs.rs
+++ rust/src/user/structs.rs
@@ -1,23 +1,49 @@
 use chrono::{DateTime, Utc, Duration};
-use std::net::SocketAddr;
-
-
+use std::net::{SocketAddr, SocketAddrV4};
+use postgres::Row;
+use deadpool::managed::Object;
+use deadpool_postgres::Manager;
 
 pub struct User {
+    id: i64,
     pub username: String,
-    pub profile_pic_s3_key: String, // The S3 key of their pfp image. Assumed to be in public bucket.
-    pub discord_info: Option<UserOauth2>
+    //pub profile_pic_s3_key: String, // The S3 key of their pfp image. Assumed to be in public bucket. // TODO
 }
 
 pub struct UserSession {
     pub user: User,
     pub creation_time: DateTime<Utc>,
-    pub session_ip: SocketAddr,
+    //pub session_ip: SocketAddr, // TODO: Implement
+    /// A string representing this specific session. Long enough to be somewhat secure.
+    pub session_id: String 
 }
 
 pub struct UserOauth2 {
     // TODO: Fill this in
     authorization_code: String
+}
+
+impl User {
+    /// Given a user ID, returns the user, if it exists.
+    pub async fn get_by_id(db_connection: Object<Manager>, given_id: &str) -> Option<Self> {
+        let query = "SELECT * FROM site_user WHERE id=$1";
+
+        let resulted_row = db_connection.query_opt(query, &[&given_id])
+                .await.unwrap(); // Can unwrap here because ID uniqueness enforced by DB.
+        
+        match resulted_row {
+            None => None,
+            Some(row) => Some(Self::from_row(row).await)
+        }
+    }
+
+    /// Given a valid site_user row, converts it to a User struct.
+    async fn from_row(row: Row) -> Self {
+        Self {
+            id: row.get("id"),
+            username: row.get("username"),
+        }
+    }
 }
 
 
@@ -30,5 +56,29 @@ impl UserSession {
         self.creation_time.checked_add_signed(SESSION_TIME_TO_EXPIRE)
             .unwrap() // This'll only panic if we reach the year 3000. It'll be fine.
             <=  chrono::Utc::now()
+    }
+
+    /// Given a session ID, returns the session, if it exists.
+    pub async fn get_by_id(db_connection: Object<Manager>, given_id: &str) -> Option<Self> {
+        let query = "SELECT * FROM user_session WHERE session_id=$1";
+
+        let resulted_row = db_connection.query_opt(query, &[&given_id])
+                .await.unwrap(); // Can unwrap here because ID uniqueness enforced by DB.
+        
+        match resulted_row {
+            None => None,
+            Some(row) => Some(Self::from_row(db_connection, row).await)
+        }
+    }
+
+    /// Given a valid user_session row, converts it to a UserSession struct.
+    async fn from_row(db_connection: Object<Manager>, row: Row) -> Self {
+        Self {
+            // Existence of parent user is enforced by DB, unwrap allowed.
+            user: User::get_by_id(db_connection, row.get("user_id")).await.unwrap(),
+            session_id: row.get("session_id"),
+            creation_time: row.get("creation_time"),
+            //session_ip:  // TODO
+        }
     }
 }
