043fc290191f04c3f28003f66da22b4ad18b099c
optimisticlucio <73440828+optimisticlucio@users.noreply.github.com>
2026-02-08 21:59:37 +0200

    the final correction

--- rust/src/utils/sql.rs
+++ rust/src/utils/sql.rs
@@ -2,16 +2,11 @@ use postgres_types::{FromSql, ToSql};
 use serde::Deserialize;
 
 /// Enum representing the state of various user posts, like art, characters, and stories.
-#[derive(Clone, FromSql, ToSql, Deserialize, Debug)]
+#[derive(Clone, FromSql, ToSql, Deserialize, Debug, Default)]
 #[postgres(name = "post_state", rename_all = "snake_case")]
 pub enum PostState {
-    Public,          // Publicly viewable, standard state.
+    #[default]
+    Public, // Publicly viewable, standard state.
     PendingApproval, // User-uploaded, pending admin review to be moved to public. Not visible.
     Processing,      // Currently mid-process by the server and/or database. Should not be viewable.
-}
-
-impl Default for PostState {
-    fn default() -> Self {
-        Self::Public
-    }
 }
