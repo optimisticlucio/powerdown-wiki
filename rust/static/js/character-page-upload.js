c6f17097aa7b7dc03a9779f9bed01431f931706d
optimisticlucio <73440828+optimisticlucio@users.noreply.github.com>
2026-02-12 22:33:24 +0200

    worked a little on the logic

--- rust/static/js/character-page-upload.js
+++ rust/static/js/character-page-upload.js
@@ -5,13 +5,56 @@
 // property called "key" pointing to the image's current URL.
 
 // Initialize only if not already defined in the page
-if (typeof filesInImageContainer === 'undefined') {
+if (typeof characterImageFiles === 'undefined') {
     // An object with the following properties that are built throughout:
     // thumbnail, logo, pageImg
     characterImageFiles = {};
 }
 
+const containers = {
+    thumbnail: document.getElementById("characterThumbnail"),
+    logo: document.getElementById("characterLogo"),
+    pageImg: document.getElementById("characterImage")
+}
+
+Object.keys(characterImageFiles).forEach((propertyName) => {
+    const img = document.createElement('img');
+    img.src = characterImageFiles[propertyName].key;
+    containers[targetImageKey].appendChild(img);
+});
+
 /// Given text, puts it in the Error Text div.
 function updateErrorText(text) {
     document.getElementById("errorDisplay").innerHTML = text;
+}
+
+// Ran when the user selects a new image file.
+function setImageFile(event, targetImageKey) {
+    const file = event.target.files[0];
+
+    // We assume it's one of the allowed filetypes. If you broke it, not my fuckin problem, this is the client.
+
+    if (file) {
+        characterImageFiles[targetImageKey] = {
+            state: "local",
+            file: file
+        };
+
+        // Now, make the user see the new thumbnail listed.
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            // Create an img element
+            const img = document.createElement('img');
+            img.src = e.target.result; // This is the base64 data URL
+
+            // Clear out any existing thumbnail visual.
+            containers[targetImageKey].innerHTML = '';
+            // Add it to the page
+            containers[targetImageKey].appendChild(img);
+        };
+
+        reader.readAsDataURL(file); // Read as data URL for images
+        event.target.value = '';
+    }
 }
